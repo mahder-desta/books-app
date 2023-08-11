d99fccacabd27c716ebf73d814e3c293621352f1
Mahder Desta <mahderds2013@gmail.com>
2023-08-11 09:50:20 +0300

    tailwind configured screen size and colors

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: {
+          50: '#D9D9D9',
+          100: '#F7F8FA',
+          200: '#EDEEF3',
+          400: '#AFB9CA',
+          500: '#919EB6',
+          700: '#5A6B87',
+          900: '#313B49',
+          1000: '#1D232B',
+          1100: '#080A0C',
+        },
+        error: {
+          300: '#FBB0AE',
+          500: '#FF003E',
+        },
+        warning: {
+          300: '#FFDCA9',
+        },
+        success: {
+          500: '#01B99F',
+        },
+      },
+
+      screens: {
+        sm: '350px',
+        lg: '700px',
+      },
+    },
+  },
+  plugins: [],
+};
